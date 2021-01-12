3fd0206bd35997ae34dcfcad3a7b1c3048455369
Antonio Carlini <antonio@acarlini.com>
2021-01-12 22:55:55 +0000

    feat: Make the script that builds the final ISO more controllable.
    unattended/scripts/build-unattended-iso.sh:
     - Allow the caller to specify the root of the build and the location of the built ISO
     - Specify UTF-8 as the character set
     - Make the build quiet (rather than screenfuls of noise)
    
    The entire build is relatively quick (less than 20s for me with an SSD) but perhaps some indication that it is working might prove useful. For now, this will do.

--- unattended/scripts/build-unattended-iso.sh
+++ unattended/scripts/build-unattended-iso.sh
@@ -1,13 +1,61 @@
 #!/bin/sh
 #
-# This script builds a bootable ISO. It expects to be invoked the directory that
-# contains everything that should be included in the ISO.
+# This script builds a bootable ISO. 
+# Options:
 #
+# --source SOURCE
+#    Specifies that SOURCE is root directory to be used when building the unattended ISO.
+#    Defaults to using the current working directory as the source.
+#    If the CDW is not the root of the copied LM ISO file then mkisofs will probably fail.
+#
+# --target TARGET
+#    Specifies the filename of the generated unattended ISO
+
 # NOTE: this is currently a work-in-progress
+
+usage()
+{
+    echo "Usage: `basename "$0"` [--source SOURCE] [--target TARGET]"
+    echo " SOURCE: the directory that contains the files that should be built into an ISO (defaults to the current directory)"
+    echo " TARGET: the filename of the ISO to create (defaults to /tmp/mint-aio.iso)"
+}
+
+original_command_line="$@"                    # save original command line so it can be printed later (after cli parsing changes the args)
+
+source="`pwd`"
+target="/tmp/mint-aio.iso"
+
+unrecognised_option=0
+while [ $# -gt 0 ]
+do
+    case "$1" in
+	-s|--source)
+	    source="$2"
+	    shift; shift # remove arg and value
+	    ;;
+	-t|--target)
+	    target="$2"
+	    shift; shift # remove arg and value
+	    ;;
+	*) # unknown option
+	    echo "Unrecognised option: [$1]"
+	    shift # remove unrecognised option
+	    unrecognised_option=1
+	    ;;
+    esac
+done
+
+if [ "${unrecognised_option}" != "0" ]; then
+    echo "At least one unrecognised option was seen: ${original_command_line}"
+    usage
+    exit
+fi
 
 mkisofs -D -r -V "AIO_MINT" -cache-inodes -J -l \
 	-b isolinux/isolinux.bin \
 	-c isolinux/boot.cat \
 	-no-emul-boot -boot-load-size 4 -boot-info-table \
-	-o /tmp/linux-mint-20.1-unattended-install.iso \
-	/tmp/mint
+	-input-charset utf-8 \
+	-quiet \
+	-o "${target}" \
+	"${source}"
